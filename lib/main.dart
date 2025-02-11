eec754517a48ab8aaef6f79fbf2a810038ee7d47
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2025-02-11 09:21:55 +0200

    handle firebase errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:ai_weather_app/ai_weather_app.dart';
+import 'package:ai_weather_app/core/utils/my_bloc_observer.dart';
+import 'package:ai_weather_app/core/utils/service_locator.dart';
 import 'package:ai_weather_app/firebase_options.dart';
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -8,5 +11,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setupLocator();
+  Bloc.observer = MyBlocObserver();
   runApp(const AiWeatherApp());
 }
