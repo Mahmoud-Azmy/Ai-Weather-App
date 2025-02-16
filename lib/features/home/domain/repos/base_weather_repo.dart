6a9c099a26ade98fc278a47b3b4c290145cdf637
Mahmoud Azmy <144074729+Mahmoud-Azmy@users.noreply.github.com>
2025-02-16 15:37:34 +0200

    -add BaseWeatherRepo

---
+++ lib/features/home/domain/repos/base_weather_repo.dart
@@ -0,0 +1,8 @@
+import 'package:ai_weather_app/core/errors/dio_errors.dart';
+import 'package:ai_weather_app/features/home/domain/entities/weather_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class BaseWeatherRepo {
+  Future<Either<Failure, List<WeatherEntity>>> getWeatherData(
+      {required String city});
+}
