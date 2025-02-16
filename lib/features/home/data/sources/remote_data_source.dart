8ff27927fd82fc9577c3ef1a0047395ee797f705
Mahmoud Azmy <144074729+Mahmoud-Azmy@users.noreply.github.com>
2025-02-16 15:38:02 +0200

    RemoteDataSource

---
+++ lib/features/home/data/sources/remote_data_source.dart
@@ -0,0 +1,27 @@
+import 'package:ai_weather_app/core/network/api_service.dart';
+import 'package:ai_weather_app/features/home/data/models/weather_model/weather_model.dart';
+import 'package:ai_weather_app/features/home/domain/entities/weather_entity.dart';
+
+abstract class BaseRemoteDataSource {
+  Future<List<WeatherEntity>> getWeatherData({required String city});
+}
+
+class RemoteDataSourceImpl extends BaseRemoteDataSource {
+  final ApiService apiService;
+
+  RemoteDataSourceImpl(this.apiService);
+  @override
+  Future<List<WeatherEntity>> getWeatherData({required String city}) async {
+    var data = await apiService.get(city);
+    List<WeatherEntity> weatherList = parseWeatherData(data);
+    return weatherList;
+  }
+
+  List<WeatherEntity> parseWeatherData(Map<String, dynamic> data) {
+    List<WeatherEntity> weatherList = [];
+    for (var item in data['forecast']['forecastday']) {
+      weatherList.add(WeatherModel.fromJson(item));
+    }
+    return weatherList;
+  }
+}
